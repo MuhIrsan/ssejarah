e57d31fd567d75e30f0962e7b0686a238e5a9bf6
MuhIrsan <78144495+MuhIrsan@users.noreply.github.com>
2021-01-28 18:33:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Jackson Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	<div id="colorlib-page">
+		<div class="container-wrap">
+		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+		<aside id="colorlib-aside" role="complementary" class="border js-fullheight">
+			<div class="text-center">
+				<div class="author-img" style="background-image: url(images/about.jpg);"></div>
+				<h1 id="colorlib-logo"><a href="index.html">WELCOME</a></h1>
+			</div>
+			<nav id="colorlib-main-menu" role="navigation" class="navbar">
+				<div id="navbar" class="collapse">
+					<ul>
+						<li class="active"><a href="#" data-nav-section="home">Home</a></li>
+						<li><a href="#" data-nav-section="about">About</a></li>
+						<li><a href="#" data-nav-section="services">Services</a></li>
+					</ul>
+				</div>
+			</nav>
+
+			
+		</aside>
+
+		<div id="colorlib-main">
+			<section id="colorlib-hero" class="js-fullheight" data-section="home">
+				<div class="flexslider js-fullheight">
+					<ul class="slides">
+				   	<li style="background-image: url(images/img_bg_1.jpg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+					   				<div class="slider-text-inner js-fullheight">
+					   					<div class="desc">
+						   					<h1><br>IRWAN</h1>
+						   					</div>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li>
+				   	
+				  	</ul>
+			  	</div>
+			</section>
+
+			<section class="colorlib-about" data-section="about">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+								<div class="col-md-12">
+									<div class="about-desc">
+										<span class="heading-meta">About</span>
+										<h2 class="colorlib-heading">BARCELONA</h2>
+										<p><strong>Sejarah buruk Barcelona</strong> Momen pergantian tahun kerap menghadirkan mimpi buruk bagi Barcelona. Klub kebanggaan publik Catalan memiliki kecenderungan tersandung dalam pertandingan pertama selepas perayaan tahun baru.
+
+Penyakit ini seolah menyerang Barcelona memasuki milenium ketiga alias 2000-an. Ambil contoh enam tahun terakhir saat keok dari Real Sociedad (0-1; 2015), imbang melawan seteru sekota, Espanyol (0-0; 2016), lalu Villarreal (1-1; 2017), dan Espanyol lagi (2-2; 2020).
+
+Baca selengkapnya: https://www.indosport.com/sepakbola/20210104/sejarah-tahun-baru-kelabu-barcelona-babak-belur-dihajar-tim-kurcaci</p>
+										<p>Barcelona (4-3-1-2): 1-Rustu; 2-Reiziger, 4-Marquez (20-Quaresma 66'), 5-Puyol, 12-Van Bronckhorst; 24-Iniesta, 6-Xavi, 8-Cocu; 10-Ronaldinho; 21-Luis Enrique, 36-Sergio Garcia (7-Saviola 75')
+
+Baca selengkapnya: https://www.indosport.com/sepakbola/20210104/sejarah-tahun-baru-kelabu-barcelona-babak-belur-dihajar-tim-kurcaci</p>
+									</div>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+									<div class="hire">
+										<h2>TEKNIK INFORMATKA A<br></h2>
+										
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+			
+			<section class="colorlib-services" data-section="services">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span class="heading-meta">What I do?</span>
+							<h2 class="colorlib-heading">sejarah singkat barcelona</h2>
+						</div>
+					</div>
+					<div class="row row-pt-md">
+						<div class="col-md-4 text-center animate-box">
+							<div class="services color-1">
+								<span class="icon">
+									<i class="icon-bulb"></i>
+								</span>
+								<div class="desc">
+									<h3>Nama pendukung barcelona</h3>
+									<p>Julukan cules untuk pendukung Barcelona berasal dari cul Katalan (bahasa Inggris: ass), sebagai penonton di stadion pertama, Camp de la Industria, duduk dengan culs mereka atas berdiri. di Spanyol, sekitar 25% dari populasi dikatakan Barca simpatisan, kedua di belakang Real Madrid, didukung oleh 32% dari populasi. Sepanjang Eropa, Barcelona adalah favorit klub pilihan kedua . </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 text-center animate-box">
+							<div class="services color-2">
+								<span class="icon">
+									<i class="icon-data"></i>
+								</span>
+								<div class="desc">
+									<h3>Rivalitas club</h3>
+									<p>Dalam sebuah liga nasional di suatu negara, sering terdapat persaingan sengit antara dua tim terkuat, dan ini terutama terjadi di La Liga, di mana pertandingan antara Real Madrid dan Barcelona dikenal sebagai "Pertemuan Klasik" (El Clásico). Sejak awal kompetisi nasional dimulai, </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 text-center animate-box">
+							<div class="services color-3">
+								<span class="icon">
+									<i class="icon-phone3"></i>
+								</span>
+								<div class="desc">
+									<h3>Prestasi club</h3>
+									<p>Hingga Mei 2016, Barcelona telah memenangkan 26 La Liga, 30 Copa del Rey, 13 Supercopa de España, 3 Copa Eva Duarte dan 2 piala Copa de la Liga, serta menjadi pemegang rekor untuk empat kompetisi terakhir. Juga mereka telah memenangkan 5 Liga Champions, rekor 4 Piala Winners UEFA, 5 Piala Super UEFA dan tiga rekor Piala Dunia Antarklub FIFA. Mereka juga memenangkan rekor 3 Piala Inter-Cities Fairs, dianggap pendahulu ke Piala UEFA/Liga Eropa.</p>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</section>
+		
+
+		</div><!-- end:colorlib-main -->
+	</div><!-- end:container-wrap -->
+	</div><!-- end:colorlib-page -->
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	
+	
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
